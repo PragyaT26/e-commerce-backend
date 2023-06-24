a3639242c0dc593a992413b4a4a0acef348723b9
Pragya Tiwari <pragyatiwari26052001@gmail.com>
2023-06-25 01:18:43 +0530

    done

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ import express from "express";
 import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
-import connectDB from "../config/db.js";
-import authRoutes from "../routes/authRoute.js";
-import categoryRoutes from "../routes/categoryRoutes.js";
-import productRoutes from "../routes/productRoutes.js";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 
 //*: configure env
